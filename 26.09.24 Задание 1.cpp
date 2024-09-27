e761847a692e45699fc0fab2a1823c4661dbd653
Nikita4501 <butakovnikita000@mail.ru>
2024-09-27 10:53:43 +0300

    Add files via upload

---
+++ 26.09.24 Задание 1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+using namespace std;
+
+int main() //Задание 1
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double a, b, c, d;
+
+    cout << "Введите длины сторон фигуры: ";
+    cin >> a >> b >> c >> d;
+
+    string type = (a == b && b == c && c == d) ? "Квадрат" :
+        (a == b && c == d) ? "Прямоугольник" :
+        (a + b > c && a + c > b && b + c > a) ? "Треугольник" :
+        "Неизвестная фигура";
+
+    cout << "Фигура: " << type << endl;
+
+    return 0;
+}
---
+++ 26.09.24 Задание 2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include<windows.h>
+#include<string>
+using namespace std;
+
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    double a, b, c;
+    cout << "Введите длины сторон треугольника: ";
+    cin >> a >> b >> c;
+
+    if (a + b > c && a + c > b && b + c > a) {
+        if (a == b && a == c) {
+            cout << "Треугольник равносторонний" << endl;
+        }
+        else if (a == b || a == c || b == c) {
+            cout << "Треугольник равнобедренный" << endl;
+        }
+        else {
+            string type = ((a * a + b * b == c * c) || (a * a + c * c == b * b) || (b * b + c * c == a * a)) ? "прямоугольный" : "разносторонний";
+            cout << "Треугольник " << type << endl;
+        }
+    }
+    else {
+        cout << "Треугольник не существует" << endl;
+    }
+
+    return 0;
+}
